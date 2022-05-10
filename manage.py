daa58ca96b44eec380811d110390c4db9534bc40
JOAO VITOR LIMA DA SILVA GUTIERRE <joao.gutierre@centralit.com.br>
2022-05-10 10:41:25 -0300

    feat:´Importando bibliotecas do arquivo de importação

--- manage.py
+++ manage.py
@@ -1,11 +1,4 @@
-from flask import Flask, request, jsonify
-import urllib3
-from random import randint
-import os
-import io
-import requests
-import json
-import time
+from imports import *
 
 urllib3.disable_warnings()
 
